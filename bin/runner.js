cb299190f34471d6443ec5f75d1cb04c2ba05504
Chad Austin <chad@chadaustin.me>
2012-06-22 17:06:42 -0700

    Wow, that was easy.  Add coffeescript support.

--- bin/runner.js
+++ bin/runner.js
@@ -3,15 +3,24 @@
 var fs = require('fs');
 var vm = require('vm');
 var imvujstest = require('../src/imvujstest.js');
+var coffeescript = require('../third-party/coffeescript-1.3.3/lib/coffee-script/coffee-script.js');
 
 function usage() {
     console.log("Please pass a test file");
     process.exit(1);
 }
 
+function endsWith(str, suffix) {
+    return str.indexOf(suffix, str.length - suffix.length) !== -1;
+};
+
 function runTest(path) {
     var testContents = fs.readFileSync(path, 'utf-8');
-    testContents = '"use strict";\n' + testContents;
+    if (endsWith(path, '.coffee')) {
+        testContents = coffeescript.compile(testContents);
+    } else {
+        testContents = '"use strict";\n' + testContents;
+    }
 
     var fn = new Function('test', 'fixture', 'assert', testContents);
     fn.call({}, imvujstest.test, imvujstest.fixture, imvujstest.assert);
