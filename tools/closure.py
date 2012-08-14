1978966df4e508d17f077eefc8b8dcf63646e500
Chad Austin <caustin@gmail.com>
2012-08-13 17:42:28 -0700

    Add a dependency on the closure compiler when using it :)

--- tools/closure.py
+++ tools/closure.py
@@ -1,7 +1,14 @@
 from SCons.Builder import Builder
 
 def generate(env):
-    ClosureCompiler = Builder(action='$JAVA $JAVAFLAGS -jar $CLOSURE_COMPILER $CLOSURE_FLAGS --js_output_file $TARGET $SOURCES')
+    def depend_on_closure_compiler(target, source, env):
+        env.Depends(target, env['CLOSURE_COMPILER'])
+        return target, source
+
+    ClosureCompiler = Builder(
+        action='$JAVA $JAVAFLAGS -jar $CLOSURE_COMPILER $CLOSURE_FLAGS --js_output_file $TARGET $SOURCES',
+        emitter=depend_on_closure_compiler
+    )
 
     env['JAVA'] = 'java'
     env['CLOSURE_COMPILER'] = env.File('#/third-party/closure-compiler/compiler.jar')
