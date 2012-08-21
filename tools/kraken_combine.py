bee15854d560e5155afa46a1b554f3be8908d8ed
Chad Austin <caustin@gmail.com>
2012-08-20 17:51:17 -0700

    Integrate automatic KrakenCombine dependencies

--- tools/kraken_combine.py
+++ tools/kraken_combine.py
@@ -1,29 +1,46 @@
 import os.path
 from SCons.Builder import Builder
+import subprocess
 
 def generate(env):
-    def depend_on_combiner(target, source, env):
+    def depend_on_combiner_and_dependencies(target, source, env):
         env.Depends(target, env['KRAKEN_COMBINE'])
+        env.Depends(target, env['KRAKEN_SCAN'])
+
+        popen = subprocess.Popen(
+            ['bash', env.subst('$KRAKEN_SCAN')] + map(str, source),
+            stdout=subprocess.PIPE)
+        stdout, _ = popen.communicate()
+        if popen.returncode:
+            raise AssertionError('scan-dependencies failed with return code %r' % (popen.returncode,))
+        env.Depends(target, filter(None, stdout.split('\n')))
+
         return target, source
 
     KrakenCombine = Builder(
         action='bash $KRAKEN_COMBINE $SOURCE > $TARGET',
-        emitter=depend_on_combiner
-    )
+        emitter=depend_on_combiner_and_dependencies)
 
     path = os.path.join(
         os.path.dirname(__file__),
         '..',
         'bin',
-        'combine'
-    )
-
+        'combine')
     path = os.path.relpath(
         os.path.normpath(path),
-        env.Dir('#').abspath
-    )
-
+        env.Dir('#').abspath)
     env['KRAKEN_COMBINE'] = path
+
+    path = os.path.join(
+        os.path.dirname(__file__),
+        '..',
+        'bin',
+        'scan-dependencies')
+    path = os.path.relpath(
+        os.path.normpath(path),
+        env.Dir('#').abspath)
+    env['KRAKEN_SCAN'] = path
+
     env.Append(
         BUILDERS={'KrakenCombine': KrakenCombine})
 
