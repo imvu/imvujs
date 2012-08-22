0e39003d8edf8f7f4d4559a4383c611923755c37
Andy Friesen <andy@imvu.com>
2012-08-22 16:38:53 -0700

    Fix tests.

--- src/polyfill.js
+++ src/polyfill.js
@@ -110,19 +110,21 @@
     if (!(console && console.log)) {
         console = polyfillConsole;
     }
-    
 
-    //ensure that we have requestAnimationFrame, or something like it
-    var polyfillRequestAnimationFrame = window.webkitRequestAnimationFrame ||
-        window.mozRequestAnimationFrame ||
-        window.oRequestAnimationFrame ||
-        window.msRequestAnimationFrame ||
-        function( callback, element ) {
-            window.setTimeout( callback, 1000/60 );
-        };
 
-    if(!window.requestAnimationFrame) {
-        window.requestAnimationFrame = polyfillRequestAnimationFrame;
+    if ('undefined' !== typeof window) {
+        var polyfillRequestAnimationFrame =
+                window.webkitRequestAnimationFrame ||
+                window.mozRequestAnimationFrame    ||
+                window.oRequestAnimationFrame      ||
+                window.msRequestAnimationFrame     ||
+                function( callback, element ) {
+                    window.setTimeout( callback, 1000/60 );
+                };
+
+        if(!window.requestAnimationFrame) {
+            window.requestAnimationFrame = polyfillRequestAnimationFrame;
+        }
     }
 
 
