eae4b83b3fc7bf1e3404bb7bc7988c9eb4c9d1b9
Chad Austin <chad@chadaustin.me>
2012-09-17 16:42:57 -0700

    Yay Set works well enough :)

--- src/Set.test.js
+++ src/Set.test.js
@@ -2,4 +2,18 @@ module({}, function() {
     test("empty set", function() {
         new Set();
     });
+
+    test("set can contain two elements", function() {
+        var a = new Set();
+        var b = [];
+        var c = {};
+        assert.notEqual(a, b);
+        a.add(b);
+        a.add(c);
+        assert.true(a.has(b));
+        assert.true(a.has(c));
+        a.delete(c);
+        assert.true(a.has(b));
+        assert.false(a.has(c));
+    });
 });
