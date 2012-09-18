67d76a5763fe6788f5d281cb853026c06a11e709
Chad Austin <chad@chadaustin.me>
2012-09-18 16:35:25 -0700

    better fixture test

--- tests/fixture.test.js
+++ tests/fixture.test.js
@@ -1,11 +1,12 @@
 module({}, function() {
     var BaseFixture = fixture("base", {
         helper: function() {
-            return 10;
+            return this.x;
         }
     });
     fixture("derived", {
         baseFixture: BaseFixture,
+        x: 10,
         "test can call base fixture": function() {
             assert.equal(10, this.helper());
         }
