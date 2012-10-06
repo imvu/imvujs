0428bf053065c1f875d88dd0d1ff0425f7f2c3e9
Chad Austin <chad@chadaustin.me>
2012-10-05 18:47:15 -0700

    Start moving FakeXMLHttpRequest over to imvujs

---
+++ fakes/FakeXMLHttpRequest.test.js
@@ -0,0 +1,93 @@
+module({
+    FakeXMLHttpRequest: 'FakeXMLHttpRequest.js'
+}, function (imports) {
+    var FakeXMLHttpRequest = imports.FakeXMLHttpRequest;
+
+    fixture('FakeXMLHttpRequest', function() {
+        this.setUp(function () {
+            FakeXMLHttpRequest.resetExpectations();
+        });
+
+        test('unexpected request', function () {
+            var xhr = new FakeXMLHttpRequest();
+            xhr.open('GET', '/foo/bar/baz');
+            assert.throws(Error, function () {
+                xhr.send();
+            });
+        });
+
+        test('expected request', function () {
+            var xhr = new FakeXMLHttpRequest();
+            xhr.open('GET', '/foo/bar/baz');
+            FakeXMLHttpRequest.expect('GET', '/foo/bar/baz', 200, {
+                "Content-Type": "text/plain"
+            }, 'Huzzah!');
+            xhr.send();
+            assert.equal(xhr.readyState, xhr.DONE);
+            assert.equal(xhr.responseText, 'Huzzah!');
+            assert.equal(xhr.getResponseHeader('Content-Type'), 'text/plain');
+        });
+
+        test('getAllResponseHeaders', function () {
+            var xhr = new FakeXMLHttpRequest();
+            xhr.open('GET', '/ok');
+            FakeXMLHttpRequest.expect('GET', '/ok', 200, {
+                a: 'b',
+                c: 'd'
+            }, 'k');
+            xhr.send();
+            assert.notEqual(xhr.getAllResponseHeaders().indexOf('a: b\r\n'), -1);
+            assert.notEqual(xhr.getAllResponseHeaders().indexOf('c: d\r\n'), -1);
+        });
+
+        test('all resolved', function () {
+            assert.true(FakeXMLHttpRequest.allResolved());
+            FakeXMLHttpRequest.expect('GET', '/foo/bar/baz', 200, {
+                "Content-Type": "text/plain"
+            }, 'Huzzah!');
+            assert.false(FakeXMLHttpRequest.allResolved());
+
+            var xhr = new FakeXMLHttpRequest();
+            xhr.open('GET', '/foo/bar/baz');
+            xhr.send();
+
+            assert.true(FakeXMLHttpRequest.allResolved());
+        });
+
+        test('readyStateChange', function () {
+            FakeXMLHttpRequest.expect('POST', '/foo/bar', 201, {
+                'x-thing': 'thang'
+            }, 'yup');
+
+            var xhr = new FakeXMLHttpRequest(),
+                states = [];
+            xhr.open('POST', '/foo/bar');
+            xhr.onreadystatechange = function () {
+                states.push(xhr.readyState);
+                if (xhr.readyState === xhr.HEADERS_RECEIVED) {
+                    assert.equal(201, xhr.status);
+                    assert.equal('thang', xhr.getResponseHeader('x-thing'));
+                }
+                if (xhr.readyState === xhr.DONE) {
+                    assert.equal('yup', xhr.responseText);
+                }
+            };
+            xhr.send();
+            assert.equal(states[0], xhr.HEADERS_RECEIVED);
+            assert.equal(states[1], xhr.LOADING);
+            assert.equal(states[2], xhr.DONE);
+            assert.equal(states.length, 3);
+        });
+
+        test('case insensitive headers', function () {
+            FakeXMLHttpRequest.expect('GET', '/foo/bar', 404, {
+                'HeaDeR': 'taco sauce'
+            }, 'yup');
+
+            var xhr = new FakeXMLHttpRequest();
+            xhr.open('GET', '/foo/bar');
+            xhr.send();
+            assert.equal(xhr.getResponseHeader('hEAdEr'), 'taco sauce');
+        });
+    });
+});
