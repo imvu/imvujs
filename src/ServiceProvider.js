f0da7a0b999a0054537ddf16d17d81bd7d0d308d
Chad Austin <chad@imvu.com>
2012-11-26 14:20:18 -0800

    can use serviceprovider to instantiate types with dependencies

--- src/ServiceProvider.js
+++ src/ServiceProvider.js
@@ -1,8 +1,21 @@
 var IMVU = IMVU || {};
 (function() {
     IMVU.ServiceProvider = BaseClass.extend('ServiceProvider', {
+        initialize: function() {
+            this.services = {};
+        },
+
+        register: function(name, instance) {
+            this.services[name] = instance;
+        },
+
         'new': function(type) {
-            return new type;
+            var options = {};
+            var services = this.services;
+            Object.keys(type.dependencies || {}).forEach(function(name) {
+                options[name] = services[name];
+            });
+            return new type(options);
         }
     });
 })();
--- src/ServiceProvider.test.js
+++ src/ServiceProvider.test.js
@@ -1,21 +1,26 @@
 fixture("ServiceProvider", function() {
+    this.setUp(function() {
+        this.sp = new IMVU.ServiceProvider;
+    });
+
     test("instantiates objects", function() {
-        var sp = new IMVU.ServiceProvider;
         function Foo(){}
-        assert['instanceof'](sp['new'](Foo), Foo);
+        assert['instanceof'](this.sp['new'](Foo), Foo);
     });
 
     test("satisfies dependencies", function() {
-/*
         var timer = {};
-        var sp = new IMVU.ServiceProvider();
-        sp.register('timer', timer);
+        this.sp.register('timer', timer);
 
         function Foo(options) {
             this.timer = options.timer;
         }
-        var instance = sp['new'](Foo);
+        Foo.dependencies = {
+            timer: {}
+        };
+        var instance = this.sp['new'](Foo);
         assert.equal(timer, instance.timer);
-*/
     });
+
+    
 });
