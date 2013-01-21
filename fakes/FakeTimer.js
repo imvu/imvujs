7c0d85c800b7033082e5f067deccea05c1fada36
Nate Downs <ndowns@imvu.com>
2013-01-21 15:35:21 -0800

    Modernize FakeTimer.

--- fakes/FakeTimer.js
+++ fakes/FakeTimer.js
@@ -5,103 +5,104 @@ module({
     // http://www.whatwg.org/specs/web-apps/current-work/multipage/timers.html#timers
 
     var START_TIME = 1234;
-    
-    function FakeTimer() {
-        this._setTime(START_TIME * 1000);
 
-        this._currentTimeoutHandle = 1;
-        this._timeouts = {};
+    return BaseClass.extend('FakeTimer', {
+        initialize: function() {
+            this._setTime(START_TIME * 1000);
 
-        this._currentIntervalHandle = 1;
-        this._intervals = {};
+            this._currentTimeoutHandle = 1;
+            this._timeouts = {};
 
-        imports.RootRegistry.addRoot(this);
-    }
+            this._currentIntervalHandle = 1;
+            this._intervals = {};
 
-    FakeTimer.prototype.getCurrentDate = function() {
-        return new Date(this.getTime());
-    };
+            imports.RootRegistry.addRoot(this);
+        },
 
-    FakeTimer.prototype.getTime = function() {
-        return this._currentTimeMsec;
-    };
 
-    FakeTimer.prototype._setTime = function (msSinceEpoch) {
-        this._currentTimeMsec = msSinceEpoch;
-    };
+        getCurrentDate: function() {
+            return new Date(this.getTime());
+        },
 
-    FakeTimer.prototype.setTimeout = function(func, timeout) {
-        var remaining = Array.prototype.slice.call(arguments, 2);
+        getTime: function() {
+            return this._currentTimeMsec;
+        },
 
-        if (!(func instanceof Function)) {
-            throw new TypeError("We don't support setTimeout with a string because it's eval.");
-        }
+        _setTime: function (msSinceEpoch) {
+            this._currentTimeMsec = msSinceEpoch;
+        },
 
-        var handle = this._currentTimeoutHandle++;
-        this._timeouts[handle] = {
-            func: function() { func.apply(undefined, remaining); },
-            endTimeMsec: this._currentTimeMsec + timeout,
-            debugString: "timeout: " + timeout + ", func: " + func.toString()
-        };
-        return handle;
-    };
+        setTimeout: function(func, timeout) {
+            var remaining = Array.prototype.slice.call(arguments, 2);
 
-    FakeTimer.prototype.clearTimeout = function(handle) {
-        delete this._timeouts[handle];
-    };
+            if (!(func instanceof Function)) {
+                throw new TypeError("We don't support setTimeout with a string because it's eval.");
+            }
 
-    FakeTimer.prototype.setInterval = function(func, timeout) {
-        var remaining = Array.prototype.slice.call(arguments, 2);
+            var handle = this._currentTimeoutHandle++;
+            this._timeouts[handle] = {
+                func: function() { func.apply(undefined, remaining); },
+                endTimeMsec: this._currentTimeMsec + timeout,
+                debugString: "timeout: " + timeout + ", func: " + func.toString()
+            };
+            return handle;
+        },
 
-        if (!(func instanceof Function)) {
-            throw new TypeError("We don't support setInterval with a string because it's eval.");
-        }
+        clearTimeout: function(handle) {
+            delete this._timeouts[handle];
+        },
 
-        var handle = this._currentIntervalHandle++;
-        this._intervals[handle] = {
-            func: function() { func.apply(undefined, remaining); },
-            timeoutMsec: timeout,
-            nextTimeMsec: this._currentTimeMsec + timeout,
-            debugString: "timeout: " + timeout + ", func: " + func.toString()
-        };
-        return handle;
-    };
+        setInterval: function(func, timeout) {
+            var remaining = Array.prototype.slice.call(arguments, 2);
 
-    FakeTimer.prototype.clearInterval = function(handle) {
-        delete this._intervals[handle];
-    };
+            if (!(func instanceof Function)) {
+                throw new TypeError("We don't support setInterval with a string because it's eval.");
+            }
 
-    FakeTimer.prototype.verifyRoot = function() {
-        for (var key in this._timeouts) {
-            var h = this._timeouts[key];
-            throw new Error("Test ended while a timeout handler was registered: " + h.debugString);
-        }
+            var handle = this._currentIntervalHandle++;
+            this._intervals[handle] = {
+                func: function() { func.apply(undefined, remaining); },
+                timeoutMsec: timeout,
+                nextTimeMsec: this._currentTimeMsec + timeout,
+                debugString: "timeout: " + timeout + ", func: " + func.toString()
+            };
+            return handle;
+        },
 
-        for (var key in this._intervals) {
-            var h = this._intervals[key];
-            throw new Error("Test ended while an interval handler was registered: " + h.debugString);
-        }
-    };
+        clearInterval: function(handle) {
+            delete this._intervals[handle];
+        },
 
-    FakeTimer.prototype._advance = function(msec) {
-        this._currentTimeMsec += msec;
+        verifyRoot: function() {
+            for (var key in this._timeouts) {
+                var h = this._timeouts[key];
+                throw new Error("Test ended while a timeout handler was registered: " + h.debugString);
+            }
 
-        for (var handle in this._timeouts) {
-            var timeout = this._timeouts[handle];
-            if (this._currentTimeMsec >= timeout.endTimeMsec) {
-                timeout.func();
-                delete this._timeouts[handle];
+            for (var key in this._intervals) {
+                var h = this._intervals[key];
+                throw new Error("Test ended while an interval handler was registered: " + h.debugString);
+            }
+        },
+
+        _advance: function(msec) {
+            this._currentTimeMsec += msec;
+
+            for (var handle in this._timeouts) {
+                var timeout = this._timeouts[handle];
+                if (this._currentTimeMsec >= timeout.endTimeMsec) {
+                    timeout.func();
+                    delete this._timeouts[handle];
+                }
+            }
+
+            for (var handle in this._intervals) {
+                var interval = this._intervals[handle];
+                if (this._currentTimeMsec >= interval.nextTimeMsec) {
+                    interval.func();
+                    interval.nextTimeMsec += interval.timeoutMsec;
+                }
             }
         }
-
-        for (var handle in this._intervals) {
-            var interval = this._intervals[handle];
-            if (this._currentTimeMsec >= interval.nextTimeMsec) {
-                interval.func();
-                interval.nextTimeMsec += interval.timeoutMsec;
-            }
-        }
-    };
-
-    return FakeTimer;
+    });
 });
