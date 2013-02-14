5b8ace28a5e425bdcf2d235dac77cdc8d3fc9cc8
Chad Austin <chad@imvu.com>
2013-02-14 13:35:37 -0800

    add an scons tool for uglify minification

--- tools/uglify.py
+++ tools/uglify.py
@@ -1,6 +1,19 @@
+import os.path
+from SCons.Builder import Builder
+
 def exists(env):
     return True
 
 def generate(env):
-    pass
+    env['UGLIFYJS'] = os.path.relpath(
+        os.path.join(
+            os.path.dirname(__file__),
+            '..',
+            'node_modules',
+            'uglify-js',
+            'bin',
+            'uglifyjs'))
+    env['UGLIFYJSFLAGS'] = []
+    env['BUILDERS']['UglifyJS'] = Builder(
+        action='$NODEJS $NODEJSFLAGS $UGLIFYJS $SOURCES -o $TARGET $UGLIFYJSFLAGS')
 
