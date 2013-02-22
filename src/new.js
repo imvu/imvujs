b54e60b244d3ecd574b137410fa92cf0a5126b39
Nate Downs <ndowns@imvu.com>
2013-02-22 14:02:25 -0800

    Create objects with IMVU.new in a way where Chrome's debugger will capture the correct name for the object.

--- src/new.js
+++ src/new.js
@@ -12,7 +12,17 @@ var IMVU = IMVU || {};
             throw new TypeError('IMVU.new called with constructor type ' + typeof(constructor) + " which is not a function");
         }
 
-        function dummy() {}
+        /*
+         * Previously, the following line was just:
+
+         function dummy() {};
+
+         * Unfortunately, Chrome was preserving 'dummy' as the object's name, even though at creation, the 'dummy' has the
+         * correct constructor name.  Thus, objects created with IMVU.new would show up in the debugger as 'dummy', which
+         * isn't very helpful.  Using IMVU.createNamedFunction addresses the issue.  Doublely-unfortunately, there's no way
+         * to write a test for this behavior.  -NRD 2013.02.22
+         */
+        var dummy = IMVU.createNamedFunction(constructor.name, function(){});
         dummy.prototype = constructor.prototype;
         var obj = new dummy;
 
