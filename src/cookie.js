4451156be0585615c28eec15dcfea499c90c017e
Andy Friesen <andy.friesen@gmail.com>
2013-07-24 16:50:06 -0700

    Tighten up IMVU.getCookies some.

--- src/cookie.js
+++ src/cookie.js
@@ -9,11 +9,11 @@ var IMVU = IMVU || {};
         return result;
     }
 
-    var r = /^([a-zA-Z0-9_-]+)=([^;]*)/;
+    var r = /^ *([^= \t\n]+)=([^;]*)/;
 
     IMVU.getCookies = function getCookies(document) {
         // I seriously cannot believe that I have to stoop to string parsing to read a cookie.
-        var cookies = document.cookie.split('; ');
+        var cookies = document.cookie.split(';');
 
         return makeObject(_.map(cookies, function (c) {
             return r.exec(c).splice(1);
