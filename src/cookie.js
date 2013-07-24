f42b9d71bd95324d168a58a77914e4509390282f
Andy Friesen <andy.friesen@gmail.com>
2013-07-24 16:53:22 -0700

    Use _.object now that we're on newer underscore.js

--- src/cookie.js
+++ src/cookie.js
@@ -1,21 +1,13 @@
 /*global IMVU:true*/
 var IMVU = IMVU || {};
 (function () {
-    function makeObject(pairs) {
-        var result = {};
-        _.each(pairs, function (p) {
-            result[p[0]] = p[1];
-        });
-        return result;
-    }
-
     var r = /^ *([^= \t\n]+)=([^;]*)/;
 
     IMVU.getCookies = function getCookies(document) {
         // I seriously cannot believe that I have to stoop to string parsing to read a cookie.
         var cookies = document.cookie.split(';');
 
-        return makeObject(_.map(cookies, function (c) {
+        return _.object(_.map(cookies, function (c) {
             return r.exec(c).splice(1);
         }));
     };
