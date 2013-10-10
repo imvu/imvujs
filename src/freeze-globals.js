a9f5e63a9b4277680d76266424dc90c55164f9b0
Chad Austin <chad@imvu.com>
2013-10-10 11:12:20 -0700

    Also freeze the global jQuery symbol.

--- src/freeze-globals.js
+++ src/freeze-globals.js
@@ -1,7 +1,7 @@
 (function() {
      // not 'IMVU' because it precludes a common idiom:
      // var IMVU = IMVU || {};
-     _.each(['$', '_', 'Backbone', 'module'], function(propertyName) {
+     _.each(['$', 'jQuery', '_', 'Backbone', 'module'], function(propertyName) {
          // configurable defaults to false
          // writable defaults to false
          Object.defineProperty(window, propertyName, {});
