9a2397dccc88bf8946497af8bb75ba48a5a3b822
Chad Austin <chad@imvu.com>
2014-01-20 18:55:20 -0800

    Add a freezeGlobalSymbols option that can be set to false to prevent imvujs from disallowing third-party jqueries and such to be loaded.

--- src/freeze-globals.js
+++ src/freeze-globals.js
@@ -1,11 +1,14 @@
 (function() {
-     // not 'IMVU' because it precludes a common idiom:
-     // var IMVU = IMVU || {};
-     _.each(['$', 'jQuery', '_', 'Backbone', 'module'], function(propertyName) {
-         Object.defineProperty(window, propertyName, {
-             configurable: false,
-             writable: false,
-             value: window[propertyName]
-         });
-     });
+    if (IMVU.freezeGlobalSymbols === undefined || IMVU.freezeGlobalSymbols) {
+        // 'IMVU' is not in the list because it precludes a common idiom:
+        // var IMVU = IMVU || {};
+        var frozenSymbols = ['$', 'jQuery', '_', 'Backbone', 'module'];
+        _(frozenSymbols).each(function(propertyName) {
+            Object.defineProperty(window, propertyName, {
+                configurable: false,
+                writable: false,
+                value: window[propertyName]
+            });
+        });
+    }
 })();
