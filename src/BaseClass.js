cc8283f9c7f7e2ad93067899b981e55528190606
joseph pea <jpea@imvu.com>
2017-02-08 13:04:11 -0800

    Make BaseClass derived class prototype.constructor property writable. This is needed in order for TypeScript `class` syntax to work in "use strict" environments. As discussed with Andy and Shamus, We want to make all code strict mode by default, and this benefit outweighs the benefit of making the constructor property writable false.

--- src/BaseClass.js
+++ src/BaseClass.js
@@ -27,14 +27,21 @@ var IMVU = IMVU || {};
         //
         //   // main.js
         //   import ClassThatExtendsBaseClass from './ClassThatExtendsBaseClass'
+        //
+        // This must be defined here because it cannot be defined inside a
+        // module due to the Object.freeze call that follows.
+        //
+        // Most imvu-style module()s export a single class, so this will
+        // automatically handle the default export for most modules. In other
+        // cases, a `default` property on the exported object would need to be
+        // specified.
         NewClass['default'] = NewClass;
 
         NewClass.prototype = Object.create(this.prototype, {
-            constructor: { value: NewClass } });
+            constructor: { value: NewClass, writable: true } });
         _.extend(NewClass.prototype, def);
 
         Object.freeze(NewClass);
-        //Object.freeze(NewClass.prototype);
 
         return NewClass;
     };
