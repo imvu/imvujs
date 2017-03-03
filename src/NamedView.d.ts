04407441fede2f230a75f3edbb2ec21813dc5d79
joseph pea <jpea@imvu.com>
2017-03-02 16:55:05 -0800

    Update typescript defs

--- src/NamedView.d.ts
+++ src/NamedView.d.ts
@@ -1,5 +1,6 @@
 // This class is created in ./NamedBackbone.js
 declare class NamedView /*TODO: extends Backbone.View*/ {
+    className: string
     static extend(name:string, def?:Object, classDef?:Object): Function // mixed in from IMVU.BaseClass
     initialize(args: Object): void
 }
