9601bf0592ec2627a9d922337d10863b8191f9a9
joseph pea <jpea@imvu.com>
2017-03-06 17:34:59 -0800

    Update NamedView type.

--- src/NamedView.d.ts
+++ src/NamedView.d.ts
@@ -1,8 +1,8 @@
+import BaseClass from 'imvu/BaseClass'
+
 // This class is created in ./NamedBackbone.js
-declare class NamedView /*TODO: extends Backbone.View*/ {
+declare class NamedView extends BaseClass /*TODO: extends Backbone.View*/ {
     className: string
-    static extend(name:string, def?:Object, classDef?:Object): Function // mixed in from IMVU.BaseClass
-    initialize(args: Object): void
 }
 
 export default NamedView
