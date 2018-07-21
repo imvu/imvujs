c94ab1593882af9853274a3469283dc76488a003
Charles Rector <crector@imvu.com>
2018-07-21 07:06:29 -0700

    Formatting

--- tests/assert.test.js
+++ tests/assert.test.js
@@ -2,14 +2,14 @@ module({
     unexpectedThrower: 'unexpectedThrower.js',
 }, function(imports) {
     fixture('new', function() {
-test("assert.throw includes actual message in error", function() {
-    var e = assert.throws(Error, function() {
-        assert.throws(ReferenceError, function() {
-            throw new TypeError('oops');
+        test("assert.throw includes actual message in error", function() {
+            var e = assert.throws(Error, function() {
+                assert.throws(ReferenceError, function() {
+                    throw new TypeError('oops');
+                });
+            });
+            assert.equal('Expected to throw "ReferenceError", actually threw: <#TypeError {}>: oops', e.message);
         });
-    });
-    assert.equal('Expected to throw "ReferenceError", actually threw: <#TypeError {}>: oops', e.message);
-});
 
         test("assert.notThrows includes wrapped stack in error", function() {
             var stack;
